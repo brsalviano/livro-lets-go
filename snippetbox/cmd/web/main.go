3eb6c825bbf1e2a00651054cfa68f417da052cb5
Bruno Salviano da Silva Teixeira <br_salviano@hotmail.com>
2021-06-28 07:23:47 -0300

    Servindo arquivos estáticos

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -12,6 +12,17 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	// Cria um servidor de arquivos que serve arquivos da pasta ./ui/static
+	// Atente-se para o fato de que o caminho passado para http.Dir é relativo a raiz do projeto.
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	//mux.Handle() está registrando o servidor de arquivos
+	//como um handler para as URLs que comecem com /static/.
+	//Para a correta correspondência dos caminhos, nós removemos o prefixo
+	//"/static" antes da requisição chegar no servidor de arquivos.
+	//Senão o caminho seria lido da forma ./ui/static/static/...
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Println("Iniciando servidor na porta 4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
