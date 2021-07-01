ac3eae3eed286afb67777a5661f738d1222f496b
Bruno Salviano da Silva Teixeira <br_salviano@hotmail.com>
2021-07-01 14:12:01 -0300

    Instância htt.Server personalizada com log de erros personalizado

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -11,18 +11,9 @@ import (
 func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
-
 	flag.Parse()
 
-	//Usa o log.New() para criar um logger para escrever mensagens informativas.
-	//Recebe 3 parâmetros: O destino para escrever os logs (os.Stout), uma string
-	//com a mensagem prefixada com a palavra INFO seguido de tab, e flags para indicar
-	//quais informações adicionais incluir (local date e time). Perceba que as flags são
-	//unidas usando o operador bitwise OR: |
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-
-	//Cria um logger para escrever mensagens de erro da mensma maneira, mas usa Stderr
-	//como destino e usa a flag log.Lshortfile para incluir o nome do arquivo relevante e o número da linha.
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	mux := http.NewServeMux()
@@ -31,12 +22,22 @@ func main() {
 	mux.HandleFunc("/snippet/create", createSnippet)
 
 	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static/")})
-
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	//Escreve as mensagens usando os 2 novos loggers.
+	//Inicializa uma nova struct http.Server. Nós configuramos o Addr e o campo Handler
+	//então o servidor vai usar o mesmo endereço da rede e rota que antes, além disso,
+	//nós configuramos o campo ErrorLog, então o servidor vai passar a usar o logger personalizado errorLog
+	//toda vez que tiver algum problema.
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
 	infoLog.Printf("Servidor escutando na porta %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
+
+	//A partir de agora vamos chamar o método ListenAndServe da instância de http.Server personalizada.
+	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
 
